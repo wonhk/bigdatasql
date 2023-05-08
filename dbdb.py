1df989366004e6523e30d91bd470aad98a34c96a
seungjune Hwang <seungjune.hwang@gmail.com>
2023-05-08 06:05:29 +0000

    sqlite3 실습

---
+++ dbdb.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+print(sqlite3.version) # 버전확인
+
+# sqlite db 파일 생성 및 연결
+con = sqlite3.connect('dbdb.db')
+print(type(con))
+cursor = con.cursor() # sql 문장을 실행시키기 위해 준비
+'''
+안녕
+여러줄로 주석 또는 문장을 
+할수 있다
+'''
+"한줄 문장"
+sql = '''
+CREATE TABLE "Person" (
+    "ID"    INTEGER NOT NULL,
+    "Name"  TEXT NOT NULL,
+    "Birthday"  TEXT,
+    PRIMARY KEY("ID" AUTOINCREMENT)
+)
+'''
+cursor.execute(sql) # sql 을 실행
